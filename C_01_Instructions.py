ac19d4cdfdf9631d2fcef7f3fd2346a1a93e5dd7
BethIrv <144862956+BethIrv@users.noreply.github.com>
2024-02-14 09:44:15 +1300

    Add files via upload

--- C_01_Instructions.py
+++ C_01_Instructions.py
@@ -19,15 +19,30 @@ def instruction():
     
 ^^^ Instructions ^^^
 
-Do something
-and then do something else
-etc
-    
+To begin, decide on a score goal (eg: The first one to get a score of 50 wins).
+
+For each round of the game, you win points by rolling dice.
+
+The winner of the round is the one who gets 13 (or slightly less)
+
+If you win the round, then your score will increase by the 
+number of points that you earned.
+
+If your first roll of two dice is a double (eg: both of the dice show a three),
+then your score will be DOUBLE the number of points.
+
+If you lose the round, then you don"t get any points.
+
+If you and the computer tie (eg: you both geta score of 11,
+then you will have 11 points added to your score.
+
     ''')
 
 
 # Main routine
+print()
 print("🎲🎲 Roll it 13 🎲🎲")
+
 
 # loop for testing purposes
 
